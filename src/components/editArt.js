6236919e00ca393f37a915ccccd9242e4fb88b0b
seungwon2 <dandelionsw2@ewhain.net>
2020-11-24 15:42:27 +0900

    :sparkles: edit page annotation setting on landing

--- src/components/editArt.js
+++ src/components/editArt.js
@@ -1,6 +1,6 @@
 /** @format */
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import Annotation from "react-image-annotation";
 import { RectangleSelector } from "react-image-annotation/lib/selectors";
@@ -13,6 +13,33 @@ import Rules from "./rules";
 
 export default function EditArt({ image }) {
 	const [annotations, setAnnotations] = useState([]);
+	useEffect(() => {
+		axios
+			.get(
+				"http://ec2-54-180-96-236.ap-northeast-2.compute.amazonaws.com:8000/api/artwork/1234/"
+			)
+			.then(({ data }) => {
+				console.log(data.annotations);
+				setAnnotations(data.annotations);
+			})
+			.catch(function (error) {
+				if (error.response) {
+					// 요청이 이루어졌으며 서버가 2xx의 범위를 벗어나는 상태 코드로 응답했습니다.
+					console.log(error.response.data);
+					console.log(error.response.status);
+					console.log(error.response.headers);
+				} else if (error.request) {
+					// 요청이 이루어 졌으나 응답을 받지 못했습니다.
+					// `error.request`는 브라우저의 XMLHttpRequest 인스턴스 또는
+					// Node.js의 http.ClientRequest 인스턴스입니다.
+					console.log("두번째" + error.request);
+				} else {
+					// 오류를 발생시킨 요청을 설정하는 중에 문제가 발생했습니다.
+					console.log("Error", error.message);
+				}
+				console.log(error.config);
+			});
+	}, []);
 	const [annotation, setAnnotation] = useState({});
 	const [activeAnnotations, setActiveAnnotations] = useState([]);
 	const [isAnnotated, setIsAnotated] = useState(false);
