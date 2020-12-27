0c88a82037ba012254d824baf36191be3ca91287
seungwon2 <dandelionsw2@ewhain.net>
2020-12-27 18:59:14 +0900

    :sparkles: server connenction finished

--- pages/add.js
+++ pages/add.js
@@ -44,7 +44,7 @@ export default function Add() {
 	const onSubmit = () => {
 		const FormData = require("form-data");
 		const artwork = new FormData();
-		artwork.append("image", ImgURL);
+		artwork.append("image", Image);
 		artwork.append("title", form.title);
 		artwork.append("artist", form.artist);
 		artwork.append("medium", form.medium);
@@ -55,29 +55,11 @@ export default function Add() {
 		artwork.append("short_desc", form.short_desc);
 		artwork.append("date", form.date);
 
-		console.log(artwork);
 		axios
 			.post("https://www.artwiki-sh.com/api/artwork/", artwork)
-			.then(function () {
-				console.log("성공");
-			})
+			.then(function () {})
 			.catch(function (error) {
 				warning();
-				if (error.response) {
-					// 요청이 이루어졌으며 서버가 2xx의 범위를 벗어나는 상태 코드로 응답했습니다.
-					console.log(error.response.data);
-					console.log(error.response.status);
-					console.log(error.response.headers);
-				} else if (error.request) {
-					// 요청이 이루어 졌으나 응답을 받지 못했습니다.
-					// `error.request`는 브라우저의 XMLHttpRequest 인스턴스 또는
-					// Node.js의 http.ClientRequest 인스턴스입니다.
-					console.log(error.request);
-				} else {
-					// 오류를 발생시킨 요청을 설정하는 중에 문제가 발생했습니다.
-					console.log("Error", error.message);
-				}
-				console.log(error.config);
 			});
 	};
 
